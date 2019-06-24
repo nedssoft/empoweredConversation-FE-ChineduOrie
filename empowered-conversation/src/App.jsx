910c29692e3e25ec0ca996aa1cebe8aeb9d21bca
Orie Chinedu <oriechinedu@gmail.com>
2019-06-25 00:20:27 +0100

    Add CSS Reset

--- empowered-conversation/src/App.jsx
+++ empowered-conversation/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import GlobalStyle from './components/styled/GlobalStyles';
+
+const AppWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 function App() {
-  return <div className="App" />;
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <AppWrapper>
+        <h1>Hello</h1>
+      </AppWrapper>
+    </React.Fragment>
+  );
 }
 
 export default App;
