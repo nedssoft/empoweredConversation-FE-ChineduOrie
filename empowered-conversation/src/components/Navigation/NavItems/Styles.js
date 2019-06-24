25b3473bd9bea5ff3e9eb1cfc8cda83247d6dbe1
Orie Chinedu <oriechinedu@gmail.com>
2019-06-25 00:55:30 +0100

    Create NavItems component

--- empowered-conversation/src/components/Navigation/NavItems/Styles.js
+++ empowered-conversation/src/components/Navigation/NavItems/Styles.js
@@ -4,3 +4,18 @@ export const Li = styled.li`
   padding:  2rem;
   display: inline-block;
 `
+export const Ul = styled.ul`
+  list-style: none;
+  position: absolute;
+  right: 200px;
+  display: flex;
+  align-items: center;
+  @media (max-width: 500px) {
+    position: relative;
+    left: 0;
+    padding: 0 1.5rem;
+    padding-bottom: 2rem;
+    flex-direction: column;
+    display: ${props => props.flex? 'flex' : 'none'};
+  }
+`
