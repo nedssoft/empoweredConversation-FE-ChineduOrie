b6a648aad0ecc74c907d2d8adb247da7c7eb556e
Orie Chinedu <oriechinedu@gmail.com>
2019-06-25 23:58:22 +0100

    Display resources retrived from the API

--- empowered-conversation/src/components/Resources/Styles.js
+++ empowered-conversation/src/components/Resources/Styles.js
@@ -1,39 +1,56 @@
 import styled from 'styled-components'
- import { flexColum } from '../styled/reusables'
+ import { flexColum, Submit } from '../styled/reusables'
 import { secondary, black } from '../styled/variables'
 
 export const Wrapper = styled.div`
   width: 60%;
-  height: 100%;
   margin: auto;
-  padding-top: 4rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin: 10rem auto;
+  min-height: 500px;
+  @media (max-width: 500px) {
+    width: 90%;
+  }
 `;
 export const Title  = styled.h2`
  font-size: 2.8rem;
  font-weight: 600;
  color: ${secondary};
+ margin: 4rem auto;
+ text-align: center;
 `
 
 export const Container = styled.div`
   ${flexColum}
   position: relative;
   h3 {
-    font-size: ${secondary};
+    color:${secondary};
+    font-size: 1.8rem;
+    align-self: flex-start;
+    position: relative;
   }
 `
 export const Author = styled.span`
-  position: absolute;
-  right: 30px;
-  bottom: 30px;
   font-style: italic;
   text-decoration: underline;
+  font-size: 1.6rem;
+  color: ${secondary};
+  align-self: flex-end;
 `
 export const Text = styled.div`
   margin: 2rem auto;
-  line-height: 1.5rem;
+  font-size: 1.6rem;
+  line-height: 1.5;
   color: ${black};
+`
+export const ActionBtn = styled(Submit)`
+  width: max-content;
+  padding: 1rem;
+  &:hover {
+    background-image: linear-gradient(to right, #24b4a5,#20c997 );
+    width: 30rem;
+  }
 `
