0d53bf0dfa1f8d1fd5e5df320febe9aaa19d62ae
Orie Chinedu <oriechinedu@gmail.com>
2019-06-25 14:36:46 +0100

    Implement a reusable modal compoenent

---
+++ empowered-conversation/src/components/UI/Modal/Modal.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import propTypes from "prop-types";
+
+import {
+  Backdrop,
+  Container,
+  Title,
+  CloseButton,
+  ContinueBtn,
+  CancelBtn,
+  Body,
+  Footer,
+  Header
+} from "./Styles";
+
+function Modal({ children, show, toggle, modalTitle, clicked, modalType }) {
+  let footerContent = <ContinueBtn onClick={clicked}>Continue</ContinueBtn>;
+  if (!modalType == "ok") {
+    footerContent = (
+      <React.Fragment>
+        <ContinueBtn onClick={clicked}>Continue</ContinueBtn>
+        <CancelBtn onClick={toggle}>Cancel</CancelBtn>
+      </React.Fragment>
+    );
+  }
+  return (
+    <React.Fragment>
+      <Backdrop show={show} onClick={toggle} />
+      <Container show={show}>
+        <Header>
+          <Title>{modalTitle}</Title>
+          <CloseButton onClick={toggle}>X</CloseButton>
+        </Header>
+        <Body>{children}</Body>
+        <Footer>{footerContent}</Footer>
+      </Container>
+    </React.Fragment>
+  );
+}
+Modal.defaultProps = {
+  show: false,
+  modalTitle: "",
+  clicked: () => {},
+  modalType: null,
+};
+Modal.propTypes = {
+  children: propTypes.node.isRequired,
+  show: propTypes.bool,
+  toggle: propTypes.func.isRequired,
+  modalTitle: propTypes.string,
+  clicked: propTypes.func,
+  modalType: propTypes.string,
+};
+export default Modal;
